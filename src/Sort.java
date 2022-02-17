99895a54e606d4b89b9b9fecfc97516120cad16a
BT_1E10_31 <sample@bths.edu>
2022-02-17 10:33:00 -0500

    implemented methods

--- src/Sort.java
+++ src/Sort.java
@@ -3,13 +3,15 @@ import java.util.ArrayList;
 public class Sort
 {
   // Part A.  Which Sort is Which?
-  public static void firstSortingAlgorithm(int[] elements)
+  public static void selectionSort(int[] elements)
   {
+    int count = 0;
     for (int j = 0; j < elements.length - 1; j++)
     {
       int minIndex = j;
       for (int k = j + 1; k < elements.length; k++)
-      {      
+      {
+        count++;
         if (elements[k] < elements[minIndex])
         {
           minIndex = k;
@@ -19,33 +21,70 @@ public class Sort
       elements[j] = elements[minIndex];
       elements[minIndex] = temp;
     }
+
+    System.out.println("Selection sort took " + count + " iterations");
   }
 
   // Part A.  Which Sort is Which?
-  public static void secondSortingAlgorithm(int[] elements)
-  { 
+  public static void insertionSort(int[] elements)
+  {
+    int count = 0;
     for (int j = 1; j < elements.length; j++)
     {
       int temp = elements[j];
       int possibleIndex = j;
       while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
-      {     
+      {
+        count++;
         elements[possibleIndex] = elements[possibleIndex - 1];
         possibleIndex--;
       }
       elements[possibleIndex] = temp;
     }
+
+    System.out.println("Insertion sort took " + count + " iterations");
   }
   
   // Part C.  Sorting a 1000-word list!
   public static void selectionSortWordList(ArrayList<String> words)
   {
-    /* TO BE IMPLEMENTED IN PART C */
+      int count = 0;
+      for (int j = 0; j < words.size() - 1; j++) {
+
+        int minIndex = j;
+
+        for (int k = j + 1; k < words.size(); k++) {
+
+          count++;
+
+          if (words.get(k).compareTo(words.get(minIndex)) < 0) {
+            minIndex = k;
+          }
+        }
+
+        String temp = words.get(j);
+        words.set(j, words.get(minIndex));
+        words.set(minIndex, temp);
+      }
+      System.out.println("Selection sort took " + count + " iterations");
   }
 
   // Part C.  Sorting a 1000-word list!
   public static void insertionSortWordList(ArrayList<String> words)
   {    
-    /* TO BE IMPLEMENTED IN PART C */
+    int count = 0;
+    for (int j = 1; j < words.size(); j++) {
+
+      String temp = words.get(j);
+      int possibleIndex = j;
+
+      while (possibleIndex > 0 && temp.compareTo(words.get(possibleIndex - 1)) < 0) {
+        words.set(possibleIndex, words.get(possibleIndex - 1));
+        possibleIndex--;
+        count++;
+      }
+      words.set(possibleIndex, temp);
+    }
+    System.out.println("Insertion sort took " + count + " iterations");
   }
 }
